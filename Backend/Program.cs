60694cf9bcf597ec4963c5319ff1c70ebb13ef67
GiuseppeGax <galiazzi.giuseppe123@gmail.com>
2025-12-11 20:31:39 -0600

    added db variable connected render

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -8,11 +8,44 @@ using System.Text;
 using MyApi.Extension;
 using Wangkanai.Detection;
 using Backend.Geo;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // DB
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// builder.Services.AddDbContext<AppDbContext>(options =>
+//     options.UseNpgsql(connectionString));
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL")
+                  ?? builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrEmpty(databaseUrl))
+{
+    throw new InvalidOperationException("Database connection string not found. Set DATABASE_URL env var or DefaultConnection in configuration.");
+}
+
+string connectionString;
+if (databaseUrl.StartsWith("postgres://", StringComparison.OrdinalIgnoreCase))
+{
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':', 2);
+    var builderN = new NpgsqlConnectionStringBuilder
+    {
+        Host = uri.Host,
+        Port = uri.Port,
+        Username = userInfo.Length > 0 ? userInfo[0] : null,
+        Password = userInfo.Length > 1 ? userInfo[1] : null,
+        Database = uri.AbsolutePath.TrimStart('/'),
+        SslMode = SslMode.Require,
+        TrustServerCertificate = true
+    };
+    connectionString = builderN.ToString();
+}
+else
+{
+    connectionString = databaseUrl;
+}
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
 
